62b23f65a94d230e3fa801772ba630ab408cfd4e
Simon Merrick <s.m3rrick@gmail.com>
2023-05-11 22:08:06 +1200

    Fix requirements, refine player prompt, add model options

--- chatrpg.py
+++ chatrpg.py
@@ -1,20 +1,25 @@
 import os
+import logging
+from argparse import ArgumentParser
 
 import openai
-
 from termcolor import colored
 from dotenv import load_dotenv
-from argparse import ArgumentParser
+
+# logging.basicConfig(loglevel=logging.ERROR)
 
 load_dotenv()
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-GPT_MODEL = "gpt-3.5-turbo"
-# GPT_MODEL="gpt-4"
-
 parser = ArgumentParser()
-parser.add_argument("--auto", dest="auto", default=False, action="store_true")
+parser.add_argument(
+    "--auto",
+    dest="auto",
+    default=False,
+    action="store_true",
+    help="Have an AI play the game for you.",
+)
 parser.add_argument(
     "--model",
     default="gpt-3.5-turbo",
@@ -26,41 +31,86 @@ parser.add_argument(
         "gpt-3.5-turbo",
         "gpt-3.5-turbo-0301",
     ],
+    help=(
+        "The default GPT Model used by both the AI generating and playing the RPG Adventure. "
+        "For information about available models see "
+        "https://platform.openai.com/docs/models/model-endpoint-compatibility"
+    ),
+)
+parser.add_argument(
+    "--player-model",
+    # default="gpt-3.5-turbo",
+    choices=[
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-32k",
+        "gpt-4-32k-0314",
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0301",
+    ],
+    help=(
+        "The GPT Model used by the AI playing the RPG Adventure. Overrides the default model. "
+        "For information about available models see "
+        "https://platform.openai.com/docs/models/model-endpoint-compatibility"
+    ),
+)
+parser.add_argument(
+    "--game-model",
+    choices=[
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-32k",
+        "gpt-4-32k-0314",
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0301",
+    ],
+    help=(
+        "The GPT Model used by the AI generating the RPG Adventure. Overrides default model "
+        "For information about available models see "
+        "https://platform.openai.com/docs/models/model-endpoint-compatibility"
+    ),
 )
 args = parser.parse_args()
+
+PLAYER_MODEL = args.player_model or args.model
+GAME_MODEL = args.game_model or args.model
 
 with open("prompt.txt", "r") as f:
     initial_prompt = "".join(f.readlines())
 
 game_messages = [{"role": "system", "content": initial_prompt}]
-
 player_messages = [
-    {"role": "system", "content": "You are playing a classic text rpg game"}
+    {
+        "role": "system",
+        "content": (
+            "You are playing a classic text rpg game. "
+            "The game will present you with a scenario. "
+            "Respond in first person with an action you take. "
+            "Do not talk about the outcome of your action. "
+            "Keep your response short. "
+        ),
+    }
 ]
 
 while True:
     # 1. Get the game prompt
-    response = openai.ChatCompletion.create(model=args.model, messages=game_messages)
-
+    response = openai.ChatCompletion.create(model=GAME_MODEL, messages=game_messages)
     response = response.choices[0]["message"]["content"]
-    print(colored(response, "green"), "\n")
-
     game_messages.append({"role": "assistant", "content": response})
+    print(colored(response, "green"), "\n")
 
     if args.auto:
         player_messages.append({"role": "user", "content": response})
 
         # 2. Get the player prompt
         response = openai.ChatCompletion.create(
-            model=args.model, messages=player_messages
+            model=PLAYER_MODEL, messages=player_messages
         )
         response = response.choices[0]["message"]["content"]
         game_messages.append({"role": "user", "content": response})
         player_messages.append({"role": "assistant", "content": response})
 
-        print(response, "\n")
-
-        # print(colored(response, 'blue'), "\n")
+        print(">", response, "\n")
     else:
         prompt = ""
         while prompt == "":
